3fd4ed159c0b22c7456ab9b9b79b7acd030ab779
Umar Farooq <ufservices465@gmail.com>
2024-08-31 14:24:08 +0530

    added animated tooltip card hover sticky scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import FeaturedCourses from "./components/FeaturedCourses";
 import HeroSection from "./components/HeroSection";
+import Instructors from "./components/Instructors";
+import MusicSchoolTestimonials from "./components/TestimonialCards";
+import UpComingWebinars from "./components/UpComingWebinars";
 import WhyChooseUs from "./components/WhyChooseUs";
 
 
@@ -10,6 +13,9 @@ export default function Home() {
    <HeroSection/>
    <FeaturedCourses/>
    <WhyChooseUs/>
+   <MusicSchoolTestimonials/>
+   <UpComingWebinars/>
+   <Instructors/>
    </main>
    </>
 
